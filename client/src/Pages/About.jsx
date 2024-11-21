e86035e0b9f0e4643538c2bbdce1eb565af2c012
Salih <infosalihv@gmail.com>
2024-11-21 11:51:37 +0530

    Home and About Page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
 const About = () => {
   return (
-    <div>
-      
+    <div className="flex items-center justify-center w-full">
+      <div className="px-4 py-12 max-w-2xl">
+        <h1 className="text-3xl font-bold mb-4 text-sky-950">ABOUT US</h1>
+        <p>
+          The aim of this project is only to learn. Understanding how to build a
+          MERN application along with Redux gave me more excitement, motivation,
+          and interest in doing the same projects. <span className="text-yellow-500 font-extrabold"> This is just the beginning!</span>
+         
+        </p>
+        <span className="text-sky-950 font-bold mt-8 block text-right">MOHAMMED SALIH V</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="flex items-center justify-center">
+      <div className="px-4 py-12 max-w-2xl">
+        <h1 className="text-3xl font-bold mb-5 text-purple-800  ">
+          Welcome to CRUD Application
+        </h1>
+        <p>
+          The purpose of this application is to allow users to create, update, and
+          delete accounts along with their data, managed securely using JWT
+          tokens.
+          <br /> <br />
+          This application is built with the MERN Stack and Redux. On the admin
+          side, administrators can view, search, delete, and block users. 
+        </p>
+        <br />
+        <span className="text-purple-800 font-semibold"> Thank you! 😊</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
